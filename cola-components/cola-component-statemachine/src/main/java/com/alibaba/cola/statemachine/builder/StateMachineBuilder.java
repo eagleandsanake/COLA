b0f252511190e893bcd7793ee3b5410631232fcf
吴某人 <@.com>
2024-11-11 14:06:32 +0800

    feat test

--- cola-components/cola-component-statemachine/src/main/java/com/alibaba/cola/statemachine/builder/StateMachineBuilder.java
+++ cola-components/cola-component-statemachine/src/main/java/com/alibaba/cola/statemachine/builder/StateMachineBuilder.java
@@ -11,27 +11,27 @@ import com.alibaba.cola.statemachine.StateMachine;
 public interface StateMachineBuilder<S, E, C> {
     /**
      * Builder for one transition
-     *
+     * from 和 to 是不同的state
      * @return External transition builder
      */
     ExternalTransitionBuilder<S, E, C> externalTransition();
 
     /**
      * Builder for multiple transitions
-     *
+     * 事件的源有多个
      * @return External transition builder
      */
     ExternalTransitionsBuilder<S, E, C> externalTransitions();
     /**
      * Builder for parallel transitions
-     *
+     * 去向有多个
      * @return External transition builder
      */
     ExternalParallelTransitionBuilder<S, E, C> externalParallelTransition();
 
     /**
      * Start to build internal transition
-     *
+     * from 和 to都是一个state
      * @return Internal transition builder
      */
     InternalTransitionBuilder<S, E, C> internalTransition();
